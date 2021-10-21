8948ba44c07587db0be01025e48622830175c1a6
BarreauxAlix <barreauxalix@tutanota.com>
2021-10-21 15:33:57 +0200

    Clean up some unecessary code in LibesGUI and ScoreGUI

--- lives_gui.gd
+++ lives_gui.gd
@@ -8,15 +8,8 @@ onready var lives_counter_label: Label = $LivesCounterLabel
 
 # ---------------------------------- RUN CODE ----------------------------------
 
-func _ready() -> void:
-	self._initialize()
-
 
 # ------------------------------ DECLARE FUNCTIONS -----------------------------
-
-
-func _initialize() -> void:
-	self.lives_counter_label.text = "0"
 
 
 func _on_LivesManager_lives_set(value: int) -> void:
--- score_gui.gd
+++ score_gui.gd
@@ -8,15 +8,9 @@ onready var score_counter_label: Label = $ScoreCounterLabel
 
 # ---------------------------------- RUN CODE ----------------------------------
 
-func _ready() -> void:
-	self._initialize()
 
 
 # ------------------------------ DECLARE FUNCTIONS -----------------------------
-
-
-func _initialize() -> void:
-	self.score_counter_label.text = "0"
 
 
 func _on_ScoreManager_score_set(value: int) -> void:
