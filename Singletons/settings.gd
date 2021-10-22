7b21ab2b4972d6c911363d4d6ad134c61b5c54bd
BarreauxAlix <barreauxalix@tutanota.com>
2021-10-22 14:01:52 +0200

    import GetText translation files and add them into the Godot project settings

---
+++ Singletons/settings.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+# ----------------------------- DECLARE VARIABLES ------------------------------
+
+
+
+# ---------------------------------- RUN CODE ----------------------------------
+
+func _ready() -> void:
+	TranslationServer.set_locale(OS.get_locale())
+
+
+# ------------------------------ DECLARE FUNCTIONS -----------------------------
+
