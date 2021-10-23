54fb04f3f74c836dc6b2bcd3308b21ad17b472c8
BarreauxAlix <barreauxalix@tutanota.com>
2021-10-23 13:48:29 +0200

    Add German translations

--- Singletons/settings.gd
+++ Singletons/settings.gd
@@ -9,7 +9,8 @@ extends Node
 # ---------------------------------- RUN CODE ----------------------------------
 
 func _ready() -> void:
-	TranslationServer.set_locale(OS.get_locale())
+#	TranslationServer.set_locale(OS.get_locale())
+	TranslationServer.set_locale("de")
 
 
 # ------------------------------ DECLARE FUNCTIONS -----------------------------
