fada3fc189eed1ce64801663143970bdd1fe31af
BarreauxAlix <barreauxalix@tutanota.com>
2021-10-26 11:05:46 +0200

    add Fa working ullScreenController in the options menu

---
+++ full_screen_controller.gd
@@ -0,0 +1,22 @@
+class_name FullScreenController
+extends HBoxContainer
+
+
+# ----------------------------- DECLARE VARIABLES ------------------------------
+
+onready var check_button: CheckButton = $CheckButton
+
+# ---------------------------------- RUN CODE ----------------------------------
+
+func _ready() -> void:
+	self._initialize()
+
+
+# ------------------------------ DECLARE FUNCTIONS -----------------------------
+
+func _initialize() -> void:
+	self.check_button.pressed = OS.is_window_fullscreen()
+
+
+func _on_CheckButton_pressed() -> void:
+	OS.set_window_fullscreen(!OS.is_window_fullscreen())
