db0657d7f0c7bf45c968661e9932724f27010a12
BarreauxAlix <barreauxalix@tutanota.com>
2021-10-31 20:46:11 +0100

    Fix VolumeControllerGUI not working

--- Scripts/GUI/volume_controller.gd
+++ Scripts/GUI/volume_controller.gd
@@ -23,22 +23,25 @@ onready var audio_bus_index: int = -1
 # ---------------------------------- RUN CODE ----------------------------------
 
 
-func _enter_tree() -> void:
-	audio_bus_index = AudioServer.get_bus_index(self.audio_bus_name)
-	print("\n ENTER TREE: ")
-	print(self.name, " : Audio bus name: ", audio_bus_name)
-	print(self.name, " : Audio bus index: ", audio_bus_index)
+#func _enter_tree() -> void:
+##	audio_bus_index = AudioServer.get_bus_index(self.audio_bus_name)
+#	print("\n ENTER TREE: ")
+#	print(self.name, " : Audio bus name: ", audio_bus_name)
+#	print(self.name, " : Audio bus index: ", audio_bus_index)
 
 
 func _ready() -> void:
 #	self._initialize_asserts()
 	self._initialize()
-	
-#	audio_bus_index = AudioServer.get_bus_index(self.audio_bus_name)
+	audio_bus_index = AudioServer.get_bus_index(self.audio_bus_name)
 
 	print("\nREADY: ")
 	print(self.name, " : Audio bus name: ", audio_bus_name)
 	print(self.name, " : Audio bus index: ", audio_bus_index)
+	
+	slider.connect("value_changed", self, "_on_Slider_value_changed")
+
+#	AudioServer.set_bus_volume_db(audio_bus_index, -80)
 
 
 # ------------------------------ DECLARE FUNCTIONS -----------------------------
@@ -57,13 +60,16 @@ func _initialize() -> void:
 	self.slider.value = self.slider.min_value / 2
 
 
-func _on_Slider_value_changed(value: int) -> void:
-	print("\nSLIDER VALUE CHANGED: ")
-	print(self.name, " : Audio bus name: ", audio_bus_name)
-	print(self.name, " : Audio bus index: ", audio_bus_index)
+func _on_Slider_value_changed(value) -> void:
+#	print("\nSLIDER VALUE CHANGED: ")
+#	print(self.name, " : Audio bus name: ", audio_bus_name)
+#	print(self.name, " : Audio bus index: ", audio_bus_index)
 #	if self.slider.value == self.slider.min_value:
 	if value == self.slider.min_value:
 		AudioServer.set_bus_mute(self.audio_bus_index, true)
 	else:
 		AudioServer.set_bus_mute(self.audio_bus_index, false)
-		AudioServer.set_bus_volume_db(audio_bus_index, self.slider.value)
+#		AudioServer.set_bus_volume_db(audio_bus_index, self.slider.value)
+		AudioServer.set_bus_volume_db(audio_bus_index, value)
+
+	print("VALUE: ", value)
