758c3922eefa64edd11ba8db676920d51e08881b
BarreauxAlix <barreauxalix@tutanota.com>
2021-10-31 21:20:57 +0100

    Set the audio stream players of the Ball scene to be on the Sound Effects audio bus and make the VollumeControllerGUI script more clear and consise. Also make the VollumeControllerGUI slider auto configure by setting its current value to the AudioBus's decibel value.

--- Scripts/GUI/volume_controller.gd
+++ Scripts/GUI/volume_controller.gd
@@ -15,61 +15,39 @@ export (String) var audio_bus_name = ""
 # Node References
 onready var slider: Slider = $HSlider
 
-
-#onready var audio_bus_index: int = AudioServer.get_bus_index(self.audio_bus_name)
 onready var audio_bus_index: int = -1
 
 
 # ---------------------------------- RUN CODE ----------------------------------
 
 
-#func _enter_tree() -> void:
-##	audio_bus_index = AudioServer.get_bus_index(self.audio_bus_name)
-#	print("\n ENTER TREE: ")
-#	print(self.name, " : Audio bus name: ", audio_bus_name)
-#	print(self.name, " : Audio bus index: ", audio_bus_index)
-
-
 func _ready() -> void:
-#	self._initialize_asserts()
+	self._initialize_asserts()
 	self._initialize()
-	audio_bus_index = AudioServer.get_bus_index(self.audio_bus_name)
-
-	print("\nREADY: ")
-	print(self.name, " : Audio bus name: ", audio_bus_name)
-	print(self.name, " : Audio bus index: ", audio_bus_index)
 	
+	# warning-ignore: return_value_discarded
 	slider.connect("value_changed", self, "_on_Slider_value_changed")
-
-#	AudioServer.set_bus_volume_db(audio_bus_index, -80)
 
 
 # ------------------------------ DECLARE FUNCTIONS -----------------------------
 
 
 func _initialize_asserts() -> void:
-#	print(self.name, " : Audio bus name: ", audio_bus_name)
-#	assert(self.audio_bus_name != "")
-	pass
+	assert(self.audio_bus_name != "")
 
 
 func _initialize() -> void:
+	audio_bus_index = AudioServer.get_bus_index(self.audio_bus_name)
 	self.slider.min_value = -80
 	self.slider.max_value = 0
 	
-	self.slider.value = self.slider.min_value / 2
+#	self.slider.value = self.slider.min_value / 2
+	self.slider.value = AudioServer.get_bus_volume_db(audio_bus_index)
 
 
 func _on_Slider_value_changed(value) -> void:
-#	print("\nSLIDER VALUE CHANGED: ")
-#	print(self.name, " : Audio bus name: ", audio_bus_name)
-#	print(self.name, " : Audio bus index: ", audio_bus_index)
-#	if self.slider.value == self.slider.min_value:
 	if value == self.slider.min_value:
 		AudioServer.set_bus_mute(self.audio_bus_index, true)
 	else:
 		AudioServer.set_bus_mute(self.audio_bus_index, false)
-#		AudioServer.set_bus_volume_db(audio_bus_index, self.slider.value)
 		AudioServer.set_bus_volume_db(audio_bus_index, value)
-
-	print("VALUE: ", value)
--- Singletons/settings.gd
+++ Singletons/settings.gd
@@ -5,7 +5,9 @@ extends Node
 # ----------------------------- DECLARE VARIABLES ------------------------------
 
 
+
 # ---------------------------------- RUN CODE ----------------------------------
+
 
 func _ready() -> void:
 	TranslationServer.set_locale(OS.get_locale())
